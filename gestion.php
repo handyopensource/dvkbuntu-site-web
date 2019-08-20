6364d95653bf60961207e9e5ad1bc83c24e37c99
gillesdc <gilles.decanniere@gmail.com>
2019-08-20 13:44:40 +0200

    envoie de dossier aux admin

--- gestion.php
+++ gestion.php
@@ -13,16 +13,94 @@ else
 if(isset($_SESSION['admin']) AND $_SESSION['admin'] == 1)
 { 
 
-if(isset($_GET['tri']))
+if(isset($_GET['tri']) AND isset($_GET['tri_user']))
 {
     $tri = htmlspecialchars($_GET['tri']);
+    $tri_user = htmlspecialchars($_GET['tri_user']);
 }
 else
 {
-    header('Location: gestion.php?tri=id');
+    header('Location: gestion.php?tri=id&tri_user=id');
 }
     
+if(isset($_POST['envoi']))
+{
+    if(isset($_POST['id']) AND !empty($_POST['id']))
+    {
+        $id = intval($_POST['id']);
+        $reqactu = $bdd->prepare('SELECT * FROM utilisateurs WHERE id = ?');
+        $reqactu->execute(array($id));
+        $actuExist = $reqactu->rowCount();
 
+        if($actuExist == 1)
+        {
+            if(isset($_POST['admin']))
+            {
+                if($_POST['admin'] == "on")
+                {
+                    $admin = 1;
+                }
+                else
+                {
+                    $admin = 0;
+                }
+            }
+            else
+            {
+                $admin = 0;
+            }
+            if(isset($_POST['auteur_actu']))
+            {
+                if($_POST['auteur_actu'] == "on")
+                {
+                    $auteur_actu = 1;
+                }
+                else
+                {
+                    $auteur_actu = 0;
+                }
+            }
+            else
+            {
+                $auteur_actu = 0;
+            }
+            if(isset($_POST['auteur_doc']))
+            {
+                if($_POST['auteur_doc'] == "on")
+                {
+                    $auteur_doc = 1;
+                }
+                else
+                {
+                    $auteur_doc = 0;
+                }
+            }
+            else
+            {
+                $auteur_doc = 0;
+            }
+
+            $req = $bdd->prepare('UPDATE utilisateurs SET admin = :admin, auteur_actu = :auteur_actu, auteur_doc = :auteur_doc WHERE id = :id');
+            $req->execute(array(
+                    'admin' => $admin,
+                    'auteur_actu' => $auteur_actu,
+                    'auteur_doc' => $auteur_doc,
+                    'id' => $id
+                   ));
+            $req->closeCursor();
+
+            $victory = "Utilisateur bien modifié !";
+        }
+        else
+        {
+            $erreur = "Pas d'id valide !";     
+        }  
+    }
+    else
+    {
+        $erreur = "Pas d'id valide !";   
+    } 
+}
 ?>
 
 <!DOCTYPE HTML>
@@ -98,9 +176,9 @@ else
                                     <h2>Actualités :</h2>
                                     <table>
                                     <tr>
-                                        <th><a href="gestion.php?tri=id">ID</a></th>
-                                        <th><a href="gestion.php?tri=auteur">Auteur</a></th>
-                                        <th><a href="gestion.php?tri=titre">Titre</a></th>
+                                        <th><a href="gestion.php?tri=id&tri_user=<?php echo $tri_user; ?>">ID</a></th>
+                                        <th><a href="gestion.php?tri=auteur&tri_user=<?php echo $tri_user; ?>">Auteur</a></th>
+                                        <th><a href="gestion.php?tri=titre&tri_user=<?php echo $tri_user; ?>">Titre</a></th>
                                         <th>Description</th>
                                         <th>Image</th>
                                         <th>Valider</th>
@@ -165,9 +243,9 @@ else
                                     <h2>Documentation :</h2>
 									<table>
                                     <tr>
-                                        <th><a href="gestion.php?tri=id">ID</a></th>
-                                        <th><a href="gestion.php?tri=auteur">Auteur</a></th>
-                                        <th><a href="gestion.php?tri=titre">Titre</a></th>
+                                        <th><a href="gestion.php?tri=id&tri_user=<?php echo $tri_user; ?>">ID</a></th>
+                                        <th><a href="gestion.php?tri=auteur&tri_user=<?php echo $tri_user; ?>">Auteur</a></th>
+                                        <th><a href="gestion.php?tri=titre&tri_user=<?php echo $tri_user; ?>">Titre</a></th>
                                         <th>Description</th>
                                         <th>Image</th>
                                         <th>Valider</th>
@@ -187,9 +265,9 @@ else
                                         {
                                             $reponse = $bdd->query('SELECT * FROM documents ORDER BY id');
                                         }
-                                        $actuExist = $reponse->rowCount();
+                                        $docExist = $reponse->rowCount();
                                     
-                                        if($actuExist == 0)
+                                        if($docExist == 0)
                                         {
                                             echo "Aucune documentation pour le moment. Revenez plus tard!";
                                         }
@@ -231,7 +309,81 @@ else
                                     
                                     ?>
                                     </table>
+                                    
+                                    <hr class="major" />
+                                    
+                                    <h2>Utilisateurs :</h2>
+									<table>
+                                    <tr>
+                                        <th><a href="gestion.php?tri=<?php echo $tri; ?>&tri_user=id">ID</a></th>
+                                        <th><a href="gestion.php?tri=<?php echo $tri; ?>&tri_user=nom">Nom</a></th>
+                                        <th><a href="gestion.php?tri=<?php echo $tri; ?>&tri_user=email">Email</a></th>
+                                        <th><a href="gestion.php?tri=<?php echo $tri; ?>&tri_user=admin">Admin</a></th>
+                                        <th><a href="gestion.php?tri=<?php echo $tri; ?>&tri_user=auteur_actu">Auteur : Actu</a></th>
+                                        <th><a href="gestion.php?tri=<?php echo $tri; ?>&tri_user=auteur_doc">Auteur : Doc</a></th>
+                                        <th>Enregistrer</th>
+                                        <th>Supprimer</th>
+                                    </tr>
+                                    <?php
+                                        
+                                        if($tri_user == "nom")
+                                        {
+                                            $reponse = $bdd->query('SELECT * FROM utilisateurs ORDER BY nom');
+                                        }
+                                        elseif($tri_user == "email")
+                                        {
+                                            $reponse = $bdd->query('SELECT * FROM utilisateurs ORDER BY email');
+                                        }
+                                        elseif($tri_user == "admin")
+                                        {
+                                            $reponse = $bdd->query('SELECT * FROM utilisateurs ORDER BY admin DESC');
+                                        }
+                                        elseif($tri_user == "auteur_actu")
+                                        {
+                                            $reponse = $bdd->query('SELECT * FROM utilisateurs ORDER BY auteur_actu DESC');
+                                        }
+                                        elseif($tri_user == "auteur_doc")
+                                        {
+                                            $reponse = $bdd->query('SELECT * FROM utilisateurs ORDER BY auteur_doc DESC');
+                                        }
+                                        else
+                                        {
+                                            $reponse = $bdd->query('SELECT * FROM utilisateurs ORDER BY id DESC');
+                                        }
+                                        $userExist = $reponse->rowCount();
+                                    
+                                        if($userExist == 0)
+                                        {
+                                            echo "Aucun utilisateur pour le moment. Revenez plus tard!";
+                                        }
+                                        else
+                                        {
+                                            // On affiche chaque entrée une à une
+                                            while ($donnees = $reponse->fetch())
+                                            { ?>
+                                                
+                                                <tr>
+                                                    <form method="post">
+                                                    <td><input type="hidden" name="id" value="<?php echo $donnees['id']; ?>"/><?php echo $donnees['id']; ?></td>
+                                                    <td><?php echo $donnees['nom']; ?></td>
+                                                    <td><?php echo $donnees['email']; ?></td>
+                                                    <td><input type="checkbox" id="admin" name="admin" <?php if($donnees['admin'] == 1) { echo "checked"; }  ?>> <label for="admin">Admin</label></td>
+                                                    <td><input type="checkbox" id="auteur_actu" name="auteur_actu" <?php if($donnees['auteur_actu'] == 1) { echo "checked"; }  ?>> <label for="auteur_actu">Actu</label></td>
+                                                    <td><input type="checkbox" id="auteur_doc" name="auteur_doc" <?php if($donnees['auteur_doc'] == 1) { echo "checked"; }  ?>><label for="auteur_doc">Doc</label></td>
+                                                   
+                                                    <td><button type="submit" class="button" name="envoi">Enregistrer</button></td>
+                                                    <td><button class="button-supprimer"><a href="suppression_user.php?id=<?php echo $donnees['id']; ?>" style="color: red;">Supprimer</a></button></td>
+                                                    </form>
+                                                </tr>
+                                                
+                                            <?php
+                                            }
+                                        }
 
+                                        $reponse->closeCursor(); // Termine le traitement de la requête
+                                    
+                                    ?>
+                                    </table>
 								</section>
 
 						</div>
